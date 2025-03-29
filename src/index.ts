133467dae2b41d77b4b67331c9da8859cc4ac14c
Patrick Lin <>
2025-03-29 17:07:15 +0100

    feat: fix polyfill __dirname

--- src/index.ts
+++ src/index.ts
@@ -14,22 +14,26 @@ function shims() {
       }
 
       const s = new MagicString(code);
-      s.prepend(`
-import __path from 'path';
-import { fileURLToPath as __fileURLToPath } from 'url';
-import { createRequire as __createRequire } from 'module';
 
-const __getFilename = () => __fileURLToPath(import.meta.url);
-const __getDirname = () => __path.dirname(__getFilename());
-const __dirname = __getDirname();
-const __filename = __getFilename();
-const self = globalThis;
-const require = __createRequire(import.meta.url);
-`);
-      return {
-        code: s.toString(),
-        map: s.generateMap({ hires: true }),
-      };
+      if (code.includes('__dirname') && !code.includes('const __dirname = ')) {
+        s.prepend(`
+  import __path from 'path';
+  import { fileURLToPath as __fileURLToPath } from 'url';
+  import { createRequire as __createRequire } from 'module';
+  
+  const __getFilename = () => __fileURLToPath(import.meta.url);
+  const __getDirname = () => __path.dirname(__getFilename());
+  const __dirname = __getDirname();
+  const __filename = __getFilename();
+  const self = globalThis;
+  const require = __createRequire(import.meta.url);
+  `);
+        return {
+          code: s.toString(),
+          map: s.generateMap({ hires: true }),
+        };
+      }
+      return null;
     },
     apply: 'build',
   } satisfies Plugin;
